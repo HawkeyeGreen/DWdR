3bb27d195cb2bb1b45a0000ab0edc3718b2112f4
HawkeyeGreen <thymiana@web.de>
2018-07-17 20:51:10 +0200

    Weitere Arbeit am EffectManager

--- DWDR_SL_Client/Universum/EffectSystem/EffectManager.cs
+++ DWDR_SL_Client/Universum/EffectSystem/EffectManager.cs
@@ -9,6 +9,37 @@ namespace DWDR_SL_Client.Universum.EffectSystem
     class EffectManager
     {
         List<AbstractEffect> effects;
-        Dictionary<string, List<Modifiers.AbstractModifier>> modifiers;
+        Dictionary<string, List<List<Modifiers.AbstractModifier>>> modifiers;
+
+        public double getModifiedValue(string key, double baseValue)
+        {
+            if(modifiers.ContainsKey(key))
+            {
+                double final = baseValue;
+                double add = 0;
+
+                foreach(Modifiers.AbstractModifier mod in modifiers[key][0])
+                {
+                    add += mod.useModifier(final);
+                }
+                final += add;
+
+                add = 0;
+                foreach (Modifiers.AbstractModifier mod in modifiers[key][1])
+                {
+                    add += mod.useModifier(final);
+                }
+                final += add;
+
+                add = 0;
+                foreach (Modifiers.AbstractModifier mod in modifiers[key][2])
+                {
+                    add += mod.useModifier(baseValue);
+                }
+
+                return final;
+            }
+            return baseValue;
+        }
     }
 }
