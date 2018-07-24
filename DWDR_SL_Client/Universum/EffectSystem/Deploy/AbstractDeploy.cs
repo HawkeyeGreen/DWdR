b773de5eb5de0020b7fd3791a349498b92770f4b
HawkeyeGreen <thymiana@web.de>
2018-07-24 12:11:40 +0200

    Effectsystem... dah

---
+++ DWDR_SL_Client/Universum/EffectSystem/Deploy/AbstractDeploy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DWDR_SL_Client.Universum.EffectSystem.Deploy
+{
+    class AbstractDeploy
+    {
+        private Effects.AbstractEffect effect;
+        private Condition.AbstractCondition condition;
+    }
+}
